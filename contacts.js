7cb9cee6142a1c140d5c4fb1c765c3e8f7f272ae
YaroslavKuzero <49403383+YaroslavKuzero@users.noreply.github.com>
2020-09-09 17:43:06 +0300

    HW-02 ready

--- contacts.js
+++ contacts.js
@@ -1,7 +1,7 @@
 const fs = require('fs').promises;
 const path = require('path');
 
-class Contacs {
+class Contacts {
 
   constructor() {
     this.contactsPath = path.resolve(__dirname, 'db', 'contacts.json')
@@ -13,39 +13,91 @@ class Contacs {
   }
 
   getContactById = async (contactId) => {
-    const contacts = await fs.readFile(this.contactsPath, 'utf8');
-    const findContact = JSON.parse(contacts).filter(item => item.id === contactId)
-    if (findContact.length) {
+    const contacts = await this.listContacts();
+    const findContact = contacts.find(item => item.id === contactId)
+    if (findContact) {
       return findContact;
     }
     return 'Contact is not exist';
   }
 
+  updateContactById = async (contactId, updateData) => {
+    const contactById = await this.getContactById(contactId);
+    if (contactById === 'Contact is not exist') {
+      return ({ "message": "Not found" })
+    }
 
-  removeContact = async (contactId) => {
-    const contacts = await fs.readFile(this.contactsPath, 'utf8');
-    const newContacts = JSON.parse(contacts).filter(item => item.id !== contactId);
-    const deletedContact = JSON.parse(contacts).find(item => item.id === contactId);
+    const contacts = await this.listContacts();
+    let updatedContact;
+    const newContacts = contacts.map(item => {
+      if (item.id === contactId) {
+        item = { ...item, ...updateData }
+        updatedContact = item;
+      }
+      return item
+    })
+    // const findContact = contacts.find(item => item.id === contactId)
+    // const idx = contacts.indexOf(findContact);
+    // const updatedContact = { ...findContact, ...updateData };
+    // await contacts.splice(idx, 1, updatedContact);
+
     await fs.writeFile(this.contactsPath, JSON.stringify(newContacts));
 
-    return `The contact ${deletedContact.name} has been successfully removed!`
+    return updatedContact;
+  }
+
+
+  removeContact = async (contactId) => {
+    const contacts = await this.listContacts();
+    const deletedContact = contacts.find(item => item.id === contactId);
+    if (deletedContact === undefined) {
+      return ({ "message": "Not found" })
+    }
+    const newContacts = contacts.filter(item => item.id !== contactId);
+
+    await fs.writeFile(this.contactsPath, JSON.stringify(newContacts));
+
+    return ({ "message": "contact deleted" })
   }
 
   addContact = async (name, email, phone) => {
-    const contacts = await fs.readFile(this.contactsPath, 'utf8');
-    const contactsList = JSON.parse(contacts);
-    const id = contactsList.length ? [...contactsList].pop().id + 1 : 1;
+    const contacts = await this.listContacts();
+
+    //Checking for duplicates
+    const toArrayValues = contacts.map(item => Object.values(item));
+    let isNotDuplicate = null;
+    toArrayValues.forEach(item => {
+      if (item.includes(name)) {
+        isNotDuplicate = `name ${name}`;
+        return;
+      }
+      if (item.includes(email)) {
+        isNotDuplicate = `email ${email}`;
+        return;
+      }
+      if (item.includes(phone)) {
+        isNotDuplicate = `phone ${phone}`;
+        return;
+      }
+      isNotDuplicate = null
+    })
+    if (isNotDuplicate !== null) {
+      return `User with ${isNotDuplicate} already exist!`
+    }
+
+    //Adding new contact
+    const id = contacts.length ? [...contacts].pop().id + 1 : 1;
     const newContact = {
       id,
       name,
       email,
       phone
     };
-    contactsList.push(newContact);
+    contacts.push(newContact);
 
-    await fs.writeFile(this.contactsPath, JSON.stringify(contactsList))
-    return `The contact ${name} has been successfully saved!`;
+    await fs.writeFile(this.contactsPath, JSON.stringify(contacts))
+    return newContact;
   }
 }
 
-module.exports = new Contacs();
+module.exports = new Contacts();
--- index.js
+++ index.js
@@ -1,27 +1,99 @@
-const Contacts = require('./contacts');
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const Joi = require('joi');
 const argv = require('yargs').argv;
 
-function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case 'list':
-      Contacts.listContacts().then(data => console.table(data));
-      break;
+const Contacts = require('./contacts');
+const invokeAction = require('./actions');
 
-    case 'get':
-      Contacts.getContactById(id).then(data => console.log(data));
-      break;
+const schemaAdd = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+});
 
-    case 'add':
-      Contacts.addContact(name, email, phone).then(data => console.log(data));
-      break;
+const schemaPatch = Joi.object({
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.string(),
+});
 
-    case 'remove':
-      Contacts.removeContact(id).then(data => console.log(data));
-      break;
+const PORT = 3000;
+const app = express();
 
-    default:
-      console.warn('\x1B[31m Unknown action type!');
+app.use(express.json());
+app.use(cors());
+app.use(morgan('combined'));
+
+// @GET /api/contacts
+app.get('/api/contacts', async (request, response) => {
+  res = await Contacts.listContacts();
+  response.status(200).send(res);
+})
+
+// @GET /api/contacts /: contactId
+app.get('/api/contacts/:contactId', async (request, response) => {
+  const id = parseInt(request.params.contactId);
+  const getContact = await Contacts.getContactById(id);
+  if (getContact === 'Contact is not exist') {
+    response.status(404).send({ "message": "Not found" })
+    return;
   }
-}
+  response.status(200).send(getContact);
+})
 
-invokeAction(argv);
+// @POST /api/contacts
+app.post('/api/contacts', async (request, response) => {
+  try {
+    await schemaAdd.validateAsync(request.body);
+  }
+  catch (err) {
+    response.status(400).send({ "message": "missing required name field" })
+    return;
+  }
+  const { name, email, phone } = request.body
+  const newContact = await Contacts.addContact(name, email, phone);
+  response.status(201).send(newContact);
+})
+
+// ### @DELETE /api/contacts /: contactId
+app.delete('/api/contacts/:contactId', async (request, response) => {
+  const id = parseInt(request.params.contactId);
+  const message = await Contacts.removeContact(id)
+  if (message === { "message": "Not found" }) {
+    response.status(404).send(message)
+    return;
+  }
+  response.status(200).send(message);
+})
+
+// ### @PATCH /api/contacts /: contactId
+app.patch('/api/contacts/:contactId', async (request, response) => {
+  if (Object.keys(request.body).length === 0) {
+    response.status(400).send({ "message": "missing fields" })
+    return;
+  }
+
+  try {
+    await schemaPatch.validateAsync(request.body);
+  }
+  catch (err) {
+    response.status(400).send(err.details);
+    return;
+  }
+
+  const id = parseInt(request.params.contactId);
+  const message = await Contacts.updateContactById(id, request.body)
+  if (message === { "message": "Not found" }) {
+    response.status(404).send(message)
+    return
+  }
+  response.status(200).send(message);
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
+})
+
+// invokeAction(argv);
